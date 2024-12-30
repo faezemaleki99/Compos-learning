875f033fbf09ee04d450b6eb2c84129c5825aab6
Faeze <faezemaleki012@gmail.com>
2024-12-30 12:05:04 +0330

    add bio and companies and partners Tab

---
+++ src/app/collections/biograph/biograph.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biograph',
+  templateUrl: './biograph.component.html',
+  styleUrl: './biograph.component.css'
+})
+export class BiographComponent {
+
+}
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,17 +5,25 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
+import { BiographComponent } from './biograph/biograph.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
     CollectionsHomeComponent,
-    TableComponent
+    TableComponent,
+    BiographComponent,
+    CompaniesComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
     CollectionsRoutingModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class CollectionsModule { }
