ad5c52c71686f1692f619115377c6cef8ffdf35b
Faeze <faezemaleki012@gmail.com>
2024-12-30 18:29:46 +0330

    add item list component

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -11,4 +11,9 @@ export class ViewsHomeComponent {
     { value: 107, label: '# likes' },
     { value: 68, label: '# comments' }
   ]
+
+  items = [
+    {image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRochT3PxzSua0f9EvmHFXmPnsgVoK2Sj8CnQ&s', title: 'winter', description:'my favorite season'},
+    {image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLfQHp9TAE_jVDKg0tZ_vff2f5U7QMIJKfNA&s', title: 'spring', description:'first season'}
+  ]
 }
