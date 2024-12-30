c05976499497c705b1d98eb0b3c4b59a51149210
Faeze <faezemaleki012@gmail.com>
2024-12-30 19:06:53 +0330

    add Modules and mods componenet

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ModsRoutingModule } from './mods-routing.module';
+import { ModalComponent } from './modal/modal.component';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModalComponent,
+    ModsHomeComponent
+  ],
   imports: [
     CommonModule,
     ModsRoutingModule
